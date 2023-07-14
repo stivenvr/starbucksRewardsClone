99984c5d2ce2dcefcaf557af09dedaa50b0ecf02
stiven valencia <s.valenciar0@gmail.com>
2023-07-14 18:04:23 -0500

    change on header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
