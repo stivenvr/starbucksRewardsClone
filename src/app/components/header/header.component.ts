654753990f979ca7d2b28d61c39d55043b5c0240
stiven valencia <s.valenciar0@gmail.com>
2023-07-18 18:47:50 -0500

    making some changes on UI

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  Mycolor: string = "#000000";
 
+  changeColor():void{
+    this.Mycolor = "#008248";
+    
+  }
+  changeColorB():void{
+    this.Mycolor = "#000000";
+  }
 }
